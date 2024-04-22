e3104dd85e5f766c74aa43070785dcad37ce2828
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-04-22 15:05:41 -0300

    Maquetado footer

---
+++ src/data/links.js
@@ -0,0 +1,5 @@
+export const LINKS = {
+  linkedin: "https://www.linkedin.com/in/ignaciofernandezjeansalle/",
+  github: "https://github.com/ignacioFernandezJeansalle",
+  gmail: "ignaciofj@gmail.com",
+};
