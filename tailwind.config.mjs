f18fc179ef812104ae9d1740f050ad4d9be2f59a
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-05-07 10:47:22 -0300

    Agregado custom colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        cblue: {
+          50: "#e9f2ff",
+          100: "#d8e5ff",
+          200: "#b8cfff",
+          300: "#8dafff",
+          400: "#617fff",
+          500: "#3c51ff",
+          600: "#1b20ff",
+          700: "#1719f2",
+          800: "#1115c2",
+          900: "#171d98",
+          950: "#0e0f58",
+        },
+      },
       keyframes: {
         "kf-opacity": {
           "0%": { opacity: 0.1 },
