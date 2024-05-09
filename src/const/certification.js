7345650e72f19967e0b9907129afbf4def94f9bf
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-05-09 15:22:33 -0300

    Definicion const de certificaciones

---
+++ src/const/certification.js
@@ -0,0 +1,22 @@
+export const CERTIFICATIONS = [
+  {
+    src: "/img/certifications/202212 Coderhouse - Frontend React Js.webp",
+    alt: "Certificado Coderhouse Carrera Frontend React Js - Clasificación Top 10",
+    thumbnail: "/img/certifications/202212 Coderhouse - Frontend React Js-thumbnail.webp",
+  },
+  {
+    src: "/img/certifications/202211 Coderhouse - React Js.webp",
+    alt: "Certificado Coderhouse React Js - Clasificación Top 10",
+    thumbnail: "/img/certifications/202211 Coderhouse - React Js-thumbnail.webp",
+  },
+  {
+    src: "/img/certifications/202209 Coderhouse - Javascript.webp",
+    alt: "Certificado Coderhouse Javascript - Clasificación Top 10",
+    thumbnail: "/img/certifications/202209 Coderhouse - Javascript-thumbnail.webp",
+  },
+  {
+    src: "/img/certifications/202207 Coderhouse - Desarrollo web.webp",
+    alt: "Certificado Coderhouse Desarrollo Web - Clasificación Top 10",
+    thumbnail: "/img/certifications/202207 Coderhouse - Desarrollo web-thumbnail.webp",
+  },
+];
