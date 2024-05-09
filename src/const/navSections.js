1a85859946ca009c26e2ce18e2445792d7df8639
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-05-09 15:43:38 -0300

    Creacion const con nav sections

---
+++ src/const/navSections.js
@@ -0,0 +1,27 @@
+import IconExperienceFE from "@/icons/IconExperienceFE.astro";
+import IconProjects from "@/icons/IconProjects.astro";
+import IconCertification from "@/icons/IconCertification.astro";
+import IconAboutMe from "@/icons/IconAboutMe.astro";
+
+export const NAV_SECTIONS = [
+  {
+    label: "Experiencia",
+    href: "/#experiencia",
+    icon: IconExperienceFE,
+  },
+  {
+    label: "Proyectos",
+    href: "/#proyectos",
+    icon: IconProjects,
+  },
+  {
+    label: "Certificaciones",
+    href: "/#certificaciones",
+    icon: IconCertification,
+  },
+  {
+    label: "Sobre mí",
+    href: "/#sobre-mi",
+    icon: IconAboutMe,
+  },
+];
