ef4aa5cd790dfe9986f9604a672bd000d8fe660f
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-05-09 15:22:50 -0300

    Cambio de estilos

--- src/components/react/GalleryCertification.jsx
+++ src/components/react/GalleryCertification.jsx
@@ -1,27 +1,5 @@
 import { useState } from "react";
-
-const CERTIFICATIONS = [
-  {
-    src: "/img/certifications/202212 Coderhouse - Frontend React Js.webp",
-    alt: "Certificado Coderhouse Carrera Frontend React Js - Clasificación Top 10",
-    thumbnail: "/img/certifications/202212 Coderhouse - Frontend React Js-thumbnail.webp",
-  },
-  {
-    src: "/img/certifications/202211 Coderhouse - React Js.webp",
-    alt: "Certificado Coderhouse React Js - Clasificación Top 10",
-    thumbnail: "/img/certifications/202211 Coderhouse - React Js-thumbnail.webp",
-  },
-  {
-    src: "/img/certifications/202209 Coderhouse - Javascript.webp",
-    alt: "Certificado Coderhouse Javascript - Clasificación Top 10",
-    thumbnail: "/img/certifications/202209 Coderhouse - Javascript-thumbnail.webp",
-  },
-  {
-    src: "/img/certifications/202207 Coderhouse - Desarrollo web.webp",
-    alt: "Certificado Coderhouse Desarrollo Web - Clasificación Top 10",
-    thumbnail: "/img/certifications/202207 Coderhouse - Desarrollo web-thumbnail.webp",
-  },
-];
+import { CERTIFICATIONS } from "@/const/certification";
 
 export default function GalleryCertification() {
   const [active, setActive] = useState(0);
@@ -31,7 +9,7 @@ export default function GalleryCertification() {
   };
 
   return (
-    <article className="mt-10 flex flex-col sm:flex-row items-center sm:items-start sm:justify-center gap-4 p-4 sm:p-10 bg-gray-900/60 border border-gray-700 rounded-md">
+    <article className="mt-10 flex flex-col sm:flex-row items-center sm:items-start sm:justify-center gap-4 p-4 sm:p-10 bg-slate-200 border rounded">
       <div className="sm:order-2">
         {
           <img
@@ -42,18 +20,20 @@ export default function GalleryCertification() {
           />
         }
       </div>
-      <nav className="flex sm:flex-col gap-2 max-w-[500px] sm:max-w-[80px] md:max-w-[100px] sm:order-1">
+      <nav className="flex sm:flex-col gap-2 max-w-[400px] sm:max-w-[80px] md:max-w-[100px] sm:order-1">
         {CERTIFICATIONS.map(({ thumbnail, alt }, index) => (
-          <img
-            key={index}
-            className={`w-full h-auto aspect-[500/389] rounded cursor-pointer ${
-              index === active ? "" : "opacity-50 hover:opacity-100"
-            }`}
-            src={thumbnail}
-            alt={alt}
-            onClick={() => handleClick(index)}
-            role="button"
-          />
+          <div>
+            <img
+              key={index}
+              className={` block w-full h-auto aspect-[500/389] rounded cursor-pointer ${
+                index === active ? "" : "opacity-50 hover:opacity-100"
+              }`}
+              src={thumbnail}
+              alt={alt}
+              onClick={() => handleClick(index)}
+              role="button"
+            />
+          </div>
         ))}
       </nav>
     </article>
